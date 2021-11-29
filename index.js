9ab97055ee504b67a9a1c8f4f5ed198952c92fbe
mustafa <mltm0204@gmail.com>
2021-11-30 01:03:23 +0300

    js codes are added

--- index.js
+++ index.js
@@ -1,43 +1,42 @@
-// I             1
-// V             5
-// X             10
-// L             50
-// C             100
-// D             500
-// M             1000
+const input = document.querySelector("input").value;
+const convert = document.querySelector("button");
+const output = document.getElementById("#output");
+// console.log(input);
 
-const intToRoman = (num) => {
-    const map = {
-      M: 1000,
-      CM: 900,
-      D: 500,
-      CD: 400,
-      C: 100,
-      XC: 90,
-      L: 50,
-      XL: 40,
-      X: 10,
-      IX: 9,
-      V: 5,
-      IV: 4,
-      I: 1,
-    };
-    let result = '';
+convert.addEventListener('click', (input) => {
+
+  const map = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+  let result = '';
+  
+  for (key in map) {  
+    const repeatCounter = Math.floor(input / map[key]);
     
-    for (key in map) {  
-      const repeatCounter = Math.floor(num / map[key]);
-      
-      if (repeatCounter !== 0) {
-        result += key.repeat(repeatCounter);         
-      }
-      
-      num %= map[key];
-      
-      if (num === 0) return result;
+    if (repeatCounter !== 0) {
+      result += key.repeat(repeatCounter);         
     }
     
-    return result;
-  };
+    input %= map[key];
+    
+    if (input === 0) return result;
+  }
+  
+  return result;
+})
+
   console.log(intToRoman("21"));
 
 
